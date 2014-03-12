53973cd0c596d75d0beb64d7705db99c494b7930
Zaahir Moolla <moollaza@fastmail.fm>
2014-03-11 23:00:09 -0400

    switched to HTML Canvas, removed uneeded libs

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 	
 	var $timer  = $('#timer'),
-		$inputs = $timer.find('input'),
-		$dial = $("#progress_dial");
+	$inputs = $timer.find('input'),
+	// $dial = $("#progress_dial");
 	
-	$dial.knob({ 
-		'readOnly': true,
-		'step' : 0.01,
-		// 'font' : 'monospace'
-		'displayInput' : false
-	});
+	// $dial.knob({ 
+	// 	'readOnly': true,
+	// 	'step' : 1,
+	// 	// 'font' : 'monospace'
+	// 	'displayInput' : false
+	// });
 
 	// var
 	timer = {
@@ -21,10 +21,11 @@ $(document).ready(function() {
 	};
 	
 	var	START = 0,
-		END = 0,
-		TS = null,
-		UNITS = countdown.HOURS|countdown.MINUTES|countdown.SECONDS|countdown.MILLISECONDS,
-		TIMEOUT;
+	END = 0,
+	LENGTH = 0,
+	TS = null,
+	TIMEOUT = null,
+	UNITS = countdown.HOURS|countdown.MINUTES|countdown.SECONDS;
 
 	$inputs.on("keydown change",function(event) {
 		
@@ -49,8 +50,11 @@ $(document).ready(function() {
 			END.setHours(END.getHours() + timer.hours());
 			// END.setDate(END.getDate() + timer.days());
 			
+			LENGTH = END - START;
+			
 			// Create countdown obj
 			window.clearInterval(TS);
+			window.clearInterval(TIMEOUT);
 			TS = countdown(
 				function (ts) {
 
@@ -69,24 +73,67 @@ $(document).ready(function() {
 					// 	.show()
 					// 	.text(JSON.stringify(ts, null, '  '));
 
-					// Update progress dial
-					var percent = ($.now() - START) / (END - START) * 100;
-					percent = Math.round(percent*100)/100;
-					console.log(percent);
-					$dial.val(percent).trigger('change');
-
 					// Timer finished
 					if (ts.value < 0) {
-						$dial.val(0).trigger('change');
+						// $dial.val(0).trigger('change');
 						window.clearInterval(TS);
+						window.clearInterval(TIMEOUT);
 						return;
 					}
 				},
 				END,
 				UNITS
 			);
+
+			// Update progress dial
+			TIMEOUT = setInterval(updateUI, 1);
 		}
+		
 	}).focus(function() {
 		$(this).select();
 	});
+
+	function updateUI() {
+		var percent = ($.now() - START) / LENGTH;
+		requestAnimFrame(function() {
+			animate(percent);
+		});
+	}
+
+	// requestAnimFrame Shim
+	window.requestAnimFrame = (function(callback) {
+		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+		function(callback) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+	})();
+
+	var canvas = document.getElementById('progress'),
+		context = canvas.getContext('2d'),
+		X = canvas.width / 2,
+		Y = canvas.height / 2,
+		RADIUS = 100,
+		CIRC = Math.PI * 2,
+		QUART = Math.PI / 2;
+
+	function animate(percent) {
+		var start = -(QUART),
+		end = (CIRC * percent) - QUART;
+
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		// outer circle
+		context.beginPath();
+		context.lineWidth = 2;
+		context.strokeStyle = '#777';
+		context.arc(X, Y, RADIUS+8, start, CIRC, false);
+		context.stroke();
+		
+		// progress circle
+		context.beginPath();
+		context.lineWidth = 10;
+		context.strokeStyle = '#ad2323';
+		context.arc(X, Y, RADIUS, start, end, false);
+		context.stroke();
+	}
 });
