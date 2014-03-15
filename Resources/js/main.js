22e5869335e68c7f79b2e54378ca89a005b2eda3
Zaahir Moolla <moollaza@gmail.com>
2014-03-15 18:56:42 -0400

    Fixed bugs, reworked logic -- possibly bug-free?

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-	'use strict'
+	'use strict';
 	
-	// jQuery objs
+	// jQuery objGITs
 	var $timer  = $('#timer'),
 		$inputs = $timer.find('input'),
-		hoursInput = function () { return parseFloat( $inputs[0].value ) },
-		minsInput  = function () { return parseFloat( $inputs[1].value ) },
-		secsInput  = function () { return parseFloat( $inputs[2].value ) },
+		hoursInput = function () { return parseFloat( $inputs[0].value ); },
+		minsInput  = function () { return parseFloat( $inputs[1].value ); },
+		secsInput  = function () { return parseFloat( $inputs[2].value ); },
 		$start = $("#start"),
-		$reset = $("#reset")
+		$reset = $("#reset");
 	
 	// Constants
-	var	START = 0,
-		END = 0,
+	var	START = null,
+		END = null,
 		LENGTH = 0,
 		TS = null,
 		TIMEOUT = null,
 		SELECTED = null,
-		PAUSED = 1,
+		STOPPPED = 1,
 		UNITS = countdown.HOURS|countdown.MINUTES|countdown.SECONDS; // For countdown.js
 
 	// CANVAS 
@@ -29,54 +29,83 @@ $(document).ready(function() {
 		CIRC = Math.PI * 2,
 		QUART = Math.PI / 2;
 
-	// Reset inputs on page refresh
-	$inputs.val(0);
+	// Reset page on refresh
+	resetUI();
 
 	// 
 	// HANDLERS
 	// 
 
-	$inputs.on("keyup change",function(event) {
-
+	$inputs.keyup(function(event) {
+		
 		// Only allow digits in input boxes
-		$(event.target).val( this.value.replace(/\D+/g, '') );
+		var value = parseFloat( this.value.replace(/\D+/g, '') );
 
-		var charCode = event.which || event.keyCode;
-
-		// Make sure "Enter" key or number key pressed
-		if (charCode == 13 || !(charCode < 48 || charCode > 57)) {
-
-			START = new Date();
-			END = new Date();
-
-			// Add up timer durations
-			END.setSeconds( END.getSeconds() + secsInput() );
-			END.setMinutes( END.getMinutes() + minsInput() );
-			END.setHours( END.getHours() + hoursInput() );
-
-			LENGTH = END - START;
-
-			// Prevent counting up if "Enter"
-			// pressed before time is entered
-			if (LENGTH <= 0) { 
-				$start.addClass('disabled');
-				return false;
-			} else {
-				$start.removeClass('disabled');
-				$reset.removeClass('disabled');
-			}
-
-			// Un-focus inputs when "Enter" pressed
-			if (charCode == 13) {
-				$inputs.blur();
-				$start.click();
-			}
+		// No negative values
+		if (value < 0 || isNaN(value) ) {
+			value = 0;
 		}
 
+		// Set changed value
+		$(event.target).val( value );
+
+		// enable Start button when timer
+		// has positive value
+		$start.addClass('disabled');
+		$inputs.each(function(index, el) {
+			if (el.value > 0) {
+				$start.removeClass('disabled');
+			}
+		});
+		
+		// Un-focus inputs when "Enter" pressed
+		// restart timer with new values
+		if (event.key === "Enter") {
+			console.log("PRESSED ENTER");
+			$inputs.blur();
+			STOPPPED = 0;
+			console.log("START!");
+
+			// create new timer
+			setUI();
+		}
+
+	}).keydown(function(event) {
+
+		var value = parseFloat( this.value.replace(/\D+/g, '') ),
+			charCode = event.which || event.keyCode;
+		
+		// up arrow pressed
+		if (charCode === 38) {
+			value+= 1;
+
+		// down arrow pressed
+		} else if (charCode === 40) {
+			value-= 1;
+		}
+
+		// No negative values
+		if (value < 0 || isNaN(value) ) {
+			value = 0;
+		}
+
+		// Set changed value
+		$(event.target).val( value );
+
 	}).click(function() {
-		if (SELECTED == this) return false;
+		console.log("CLICK!");
+		// ignore if already selected,
+		// to allow cursor placement
+		if (SELECTED === this) { return false; }
 		SELECTED = this;
 		this.select();
+
+	}).change(function(event) {
+
+		console.log("Inputs changed!");
+
+		// create new timer
+		setUI();
 	});
 
 
@@ -84,19 +113,66 @@ $(document).ready(function() {
 	// progress circle
 	$start.click(function(event) {
 		
-		PAUSED = !PAUSED;
+		var continue_timer = false;
 
-		if (PAUSED){
+		$inputs.each(function(index, el) {
+			if (el.value > 0) {
+				continue_timer = true;
+			}
+		});
+
+		STOPPPED = !STOPPPED;
+		// create new timer
+		setUI(continue_timer);
+	});
+
+
+	$reset.click(function(event) {
+		resetUI();
+	});
+
+	//
+	// HELPERS
+	//
+	
+	function setUI (continue_timer) {
+
+		console.log("setUI()");
+		console.log("STOPPPED: ", STOPPPED);
+
+		if (STOPPPED) { 
 			clearTimers();
-			$start.text('Start').toggleClass('btn-success btn-danger');
+			$start.text('Start').removeClass('btn-danger').addClass('btn-success');
+			return false;
+		} 
+		
+		$start.text('Stop').removeClass('btn-success').addClass('btn-danger');
+		
+		if (!continue_timer) {
+			clearTimers();
+			START = new Date();
+		}
+
+		END = new Date();
+
+		// Add up timer durations
+		END.setSeconds( END.getSeconds() + secsInput() );
+		END.setMinutes( END.getMinutes() + minsInput() );
+		END.setHours( END.getHours() + hoursInput() );
+
+		LENGTH = END - START;
+
+		// Prevent counting up if "Enter"
+		// pressed before time is entered
+		if (LENGTH <= 0) { 
+			resetUI();
+			return false;
 		} else {
-			$start.text('Stop').toggleClass('btn-success btn-danger');
-		};
+			$start.removeClass('disabled');
+			$reset.removeClass('disabled');
+		}
 
-
-		if (PAUSED) { return false };
-
-		// Create countdown obj
+		// Create new countdown
 		TS = countdown(
 			function (ts) {
 
@@ -106,7 +182,7 @@ $(document).ready(function() {
 				});
 
 				// Timer finished
-				if (ts.value < 0) {
+				if (ts.value <= 0) {
 					clearTimers();
 					$start.text('Start').removeClass('btn-danger').addClass('btn-success disabled');
 					return;
@@ -118,35 +194,36 @@ $(document).ready(function() {
 
 		// Update progress circle
 		TIMEOUT = setInterval(updateUI, 1);
-	});
-
-	$reset.click(function(event) {
-		clearTimers();
-		$inputs.val(0);
-		$start.text('Start').removeClass('btn-danger').addClass('btn-success disabled');
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		PAUSED = 1;
-	});
-
-	//
-	// HELPERS
-	// 
-
-	// requestAnimFrame Shim
-	window.requestAnimFrame = (function(callback) {
-		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-		function(callback) {
-			window.setTimeout(callback, 1000 / 60);
-		};
-	})();
+	}
 
 	// Calculate percent completion
 	// and draw to canvas accordingly
 	function updateUI() {
 		var percent = ($.now() - START) / LENGTH;
-		requestAnimFrame(function() {
-			animate(percent);
-		});
+		console.log("PERCENT IS: %s%", Math.round(percent));
+		animate(percent);
+	}
+
+	// Reset all UI elements
+	// and clear timers
+	function resetUI() {
+	
+		// Set inputs to 0
+		$inputs.val(0);
+		
+		// Clear Canvas
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		
+		// Reset Start button
+		$start.text('Start').removeClass('btn-danger').addClass('btn-success disabled');
+
+		clearTimers();
+		
+		// reset state
+		START = null;
+		END = null;
+		STOPPPED = 1;
+		
 	}
 
 	// Clear all interval timers
@@ -158,7 +235,7 @@ $(document).ready(function() {
 	// Draw circle to canvas
 	function animate(percent) {
 		var start = -(QUART),
-			end = (CIRC * percent) - QUART;
+		end = (CIRC * percent) - QUART;
 
 		context.clearRect(0, 0, canvas.width, canvas.height);
 
